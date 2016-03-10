a7e1c0707434e717800e02acd4ab22db85df4104
Ksenia Kanishcheva <xksusha@Ksenias-MacBook-Pro.local>
2016-03-10 12:51:50 +0100

    change search page

---
+++ app/assets/javascripts/angular/controllers/SearchController.js
@@ -0,0 +1,28 @@
+App.controller('SearchCtrl',['$scope','$state', function($scope,$state){
+	
+	var monthNames = [
+	  " January ", " February ", " March ",
+	  " April ", " May ", " June ", " July ",
+	  " August ", " September ", " October ",
+	  " November ", " December "
+	];
+
+	var getResults = function(){
+		console.log("here")
+		if($scope.inherit.usertype == 'traveller'){
+			return Requests
+		}else{
+			return Proposals.map(function(d){d.date = "" + d.day + monthNames[d.month-1] + d.year; return d})
+		}
+	}
+
+	$scope.local={
+		results: getResults()
+	}
+	
+
+
+
+
+
+}]);
