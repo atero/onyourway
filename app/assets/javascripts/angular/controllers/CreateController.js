519ec5238d66b90c23485ed862382dbd02742a71
Ksenia Kanishcheva <xksusha@ksenias-mbp.eki.group>
2016-03-16 12:18:19 +0100

    start create, change routes

--- app/assets/javascripts/angular/controllers/CreateController.js
+++ app/assets/javascripts/angular/controllers/CreateController.js
@@ -8,5 +8,15 @@ App.controller('CreateCtrl',['$scope','$state', function($scope,$state){
 		$scope.local.stages=[true, false]
 	}
 
+	$scope.stepForward = function(){
+		curr_stage = $scope.local.stages.indexOf(true)
+		$scope.local.stages[curr_stage] = false
+		$scope.local.stages[curr_stage+1] = true
+	}
+	$scope.stepBack= function(){
+		curr_stage = $scope.local.stages.indexOf(true)
+		$scope.local.stages[curr_stage] = false
+		$scope.local.stages[curr_stage-1] = true
+	}
 
 }]);
