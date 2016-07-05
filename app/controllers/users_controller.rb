87aa387f53c817eb67148b85cf3630503363b2ac
Karen Chobanyan <karen@atero.solutions>
2016-07-05 11:28:38 +0400

    heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
 
   def create
+
     @user = User.new(user_params)
     if @user.save
         render json: @user, status: :accepted
+    else
+       render json: {messsage:'Bad request'}, status: 400
+    end
+
   end
 
   private
@@ -11,5 +16,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :password)
   end
-  
 end
