a7c99f2e13ee1391ef82af011934481227913d24
Karen Chobanyan <karen@atero.solutions>
2016-07-14 12:12:52 +0400

    Emails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,18 @@ class UsersController < ApplicationController
     p @user
     p user_params
     p "**************************************************************"
-    if @user.save
-      UserMailer.welcome_email(@user).deliver_later
-      render json: @user, status: :accepted
-    else
-      render json: {messsage:'Bad request'}, status: 400
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.welcome_email(@user).deliver_later
+
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
-  end
 
   private
 
