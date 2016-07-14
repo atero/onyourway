0bdb51b50bad576736cccbab96c84dbc5da98562
Gevorg Melqonyan <gevorg@Karens-Mac-Pro-3.local>
2016-07-14 11:32:29 +0400

    sign template ready to test

--- app/assets/javascripts/angular/services/services.js
+++ app/assets/javascripts/angular/services/services.js
@@ -29,15 +29,14 @@ App.factory('Message',['$resource',function($resource){
 }]);
 
 
-// App.factory('User',['$resource',function($resource){
-	
-// 	return $resource(api_endpoint+'v1/users/:id/', {id:'@id'},
-// 	{
-// 		show: { method:"GET", params: {id :'@id'}, isArray : false },
-// 		index: { method:"GET", params: {}, isArray : true },
-// 		create: { method:"POST", params: {}, isArray : false },
-// 		update: { method:"PUT", params: {id :'@id'}, isArray : false }
-// 	});
+App.factory('User',['$resource',function($resource){
 
-// }]);
+	return $resource(api_endpoint+'v1/users/:id/', {id:'@id'},
+	{
+		show: { method:"GET", params: {id :'@id'}, isArray : false },
+		index: { method:"GET", params: {}, isArray : true },
+		create: { method:"POST", params: {}, isArray : false },
+		update: { method:"PUT", params: {id :'@id'}, isArray : false }
+	});
 
+}]);
