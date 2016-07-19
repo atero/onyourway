4d98dce61a7400cbc6ef5fd670c90c4ecd7e2258
Karen Chobanyan <karen@atero.solutions>
2016-07-19 17:12:20 +0400

    Profile layout

--- app/assets/javascripts/angular/directives/resize.js
+++ app/assets/javascripts/angular/directives/resize.js
@@ -1,10 +1,9 @@
 App.directive('resize', function($window) {
-  return function (scope, element) {
-      var w = angular.element($window);
-      var changeHeight = function() {element.css('min-height', (w.height() -330) + 'px' );};
-          w.bind('resize', function () {
-            changeHeight();   // when window size gets changed
-      });
-      changeHeight(); // when page loads
+  return function(scope, element) {
+    var w = angular.element($window);
+    var changeHeight = function() {
+      element.css('min-height', (w.height() - 330) + 'px');
+    };
+    changeHeight(); // when page loads
   }
 });
