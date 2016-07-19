4a693672206bec6b472117f00ce722bcf884eebf
Karen Chobanyan <karen@atero.solutions>
2016-07-19 19:51:53 +0400

    Profile layout

--- app/assets/javascripts/angular/controllers/OrdersController.js
+++ app/assets/javascripts/angular/controllers/OrdersController.js
@@ -5,7 +5,7 @@ App.controller('OrdersCtrl',['$scope', '$state','localuser', 'orders', 'Shipment
 		orders: orders,
 		conversation_shipment: null
 	}
-
+	console.log($scope.local.orders);
 	$scope.accept_travel = function(order, shipment){
 		order.accepted_shipment = true;
 
