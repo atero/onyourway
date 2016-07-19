c0c7f50f5c1a375af18e9594923014f88b0d13bd
Karen Chobanyan <karen@atero.solutions>
2016-07-19 17:22:18 +0400

    Profile layout

--- app/assets/javascripts/angular/directives/resize.js
+++ app/assets/javascripts/angular/directives/resize.js
@@ -1,9 +1,10 @@
-App.directive('resize', function($window) {
-  return function(scope, element) {
-    var w = angular.element($window);
-    var changeHeight = function() {
-      element.css('min-height', (w.height() - 330) + 'px');
-    };
-    changeHeight(); // when page loads
-  }
-});
+angular.module("ShopNShip")
+  .directive('resize', function($window) {
+    return function(scope, element) {
+      var w = angular.element($window);
+      var changeHeight = function() {
+        element.css('min-height', (w.height() - 330) + 'px');
+      };
+      changeHeight(); // when page loads
+    }
+  });
