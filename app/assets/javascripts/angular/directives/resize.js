eecfaef2fac87ad4ba0dd58b78cfab6a5d64161d
Karen Chobanyan <karen@atero.solutions>
2016-07-19 17:08:24 +0400

    Profile layout

--- app/assets/javascripts/angular/directives/resize.js
+++ app/assets/javascripts/angular/directives/resize.js
@@ -1,7 +1,7 @@
 App.directive('resize', function($window) {
   return function (scope, element) {
       var w = angular.element($window);
-      var changeHeight = function() {element.css('height', (w.height() -330) + 'px' );};
+      var changeHeight = function() {element.css('min-height', (w.height() -330) + 'px' );};
           w.bind('resize', function () {
             changeHeight();   // when window size gets changed
       });
