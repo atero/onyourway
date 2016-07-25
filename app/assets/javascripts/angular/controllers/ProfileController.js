9c98c546886f57fe6a0bf54cd8ff167149af56ac
Karen Chobanyan <karen@atero.solutions>
2016-07-25 11:13:06 +0400

    Prelaunch no 3% oyw fee

--- app/assets/javascripts/angular/controllers/ProfileController.js
+++ app/assets/javascripts/angular/controllers/ProfileController.js
@@ -1,6 +1,7 @@
 App.controller('ProfileCtrl',['$scope','$state','localuser', function($scope,$state, localuser){
 	if (localuser) $scope.inherit.user = localuser;
 	console.log($scope.inherit.user);
+
 	$scope.local={
 
 	}
