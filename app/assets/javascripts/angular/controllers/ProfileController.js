9c24d2442d7feeec3309dc5a7be392bbb196fd2a
Karen Chobanyan <karen@atero.solutions>
2016-07-27 20:13:56 +0400

    FAQ Cond banner

--- app/assets/javascripts/angular/controllers/ProfileController.js
+++ app/assets/javascripts/angular/controllers/ProfileController.js
@@ -1,10 +1,9 @@
 App.controller('ProfileCtrl',['$scope','$state','localuser', function($scope,$state, localuser){
+
 	if (localuser) $scope.inherit.user = localuser;
 	console.log($scope.inherit.user);
 
-	$scope.local={
-
-	}
+	$scope.local={}
 
 	$scope.saveProfile = function(){
 		console.log("saving");
