57965521e1724b20c124df8dee130ce873c2e9f9
Arman Qocharyan <arman@Armans-Mac-Pro.local>
2016-08-04 16:42:09 +0400

    Travels

--- app/assets/javascripts/angular/controllers/ShipmentsController.js
+++ app/assets/javascripts/angular/controllers/ShipmentsController.js
@@ -6,4 +6,15 @@ App.controller('ShipmentsCtrl',['$scope','$state','localuser', 'shipments', func
 		shipments: shipments
 	}
 	console.log($scope.local.shipments);
+
+
+	$scope.setOrder = function(order){
+		$scope.local.conversation_shipment = null
+		$scope.local.data_to_show = order;
+		if ($scope.local.data_to_show.accepted_shipment) {
+			$scope.local.conversation_shipment = $scope.local.data_to_show.shipments.filter(function(d){return d.status == "accepted"})[0]
+		}
+	}
+
+
 }]);
