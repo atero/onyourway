738e18e5f2b4acf159c48e1d96c058573fc02e91
Karen Chobanyan <karen@atero.solutions>
2016-08-05 18:20:03 +0400

    marge with arman

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,7 +10,7 @@ class Message
    validate :users_consistency
 
   def users_consistency
-    p self.shipment
+    p self.shipment.orders.last
     u1 = self.shipment.order.user
     u2 = self.shipment.user
     if !(u1 == self.sender && u2 == self.recipient || u2 == self.sender && u1 == self.recipient)
