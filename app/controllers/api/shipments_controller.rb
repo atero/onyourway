c66fceb1535c899118baa0e383ff75df70dc0e0f
Karen Chobanyan <karen@atero.solutions>
2016-08-05 17:17:37 +0400

    marge with arman

--- app/controllers/api/shipments_controller.rb
+++ app/controllers/api/shipments_controller.rb
@@ -11,7 +11,7 @@ module Api
 
       if params[:order_id]
         @order = Order.where(:id => params["order_id"]).first
-        if @order then @shipment.order = [] end
+        if @order then @shipment.order = [params["order_id"]] end
       end
 
       if @shipment.save
