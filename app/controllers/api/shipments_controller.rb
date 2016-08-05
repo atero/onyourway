166ced335da75f3a46868bca75081ad4206f0fa2
Karen Chobanyan <karen@atero.solutions>
2016-08-05 20:27:08 +0400

    marge with arman

--- app/controllers/api/shipments_controller.rb
+++ app/controllers/api/shipments_controller.rb
@@ -1,26 +1,28 @@
 module Api
   class ShipmentsController < ApplicationController
-
     before_filter :authenticate_user!
 
     def create
-
-      @shipment = Shipment.new(shipment_params)
-      @shipment.user = current_user
-
-      if params[:order_id]
-        puts params[:order_id] + "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-        @order = Order.where(:id => params["order_id"]).first
-        if @order then @shipment.order = [@order] end
+      @ext = Shipment.where(date: params['date']).first
+      if ext
+        puts 'Exist!!!!!!!!!!!!!!!!!!'
+        @shipment = @ext
+      else
+        @shipment = Shipment.new(shipment_params)
+        @shipment.user = current_user
+        if params[:order_id]
+          puts params[:order_id] + '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^'
+          @order = Order.where(id: params['order_id']).first
+          @shipment.order = [@order] if @order
+          puts 'Order ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;'
+          puts @shipment.order
+          if @shipment.save
+            render json: @shipment, status: :accepted
+          else
+            render json: { messsage: 'Bad request' }, status: 400
+          end
+        end
       end
-      puts "Order ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;"
-      puts @shipment.order
-      if @shipment.save
-          render json: @shipment, status: :accepted
-       else
-         render json: {messsage:'Bad request'}, status: 400
-      end
-
     end
 
     def list
@@ -29,14 +31,13 @@ module Api
     end
 
     def update
-
-      @shipment = Shipment.where(:id=> params[:shipment_id]).first
+      @shipment = Shipment.where(id: params[:shipment_id]).first
 
       if @shipment && @shipment.update(shipment_params)
         @shipment.order.save
         render json: @shipment, status: :accepted
       else
-        render json: {messsage:'No orders found'}, status: 404
+        render json: { messsage: 'No orders found' }, status: 404
       end
     end
 
