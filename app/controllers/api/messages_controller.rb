44915dbe7ec956ea65806769821459f325986564
Karen Chobanyan <karen@atero.solutions>
2016-08-08 15:30:12 +0400

    shipments

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,9 +3,11 @@ module Api
 
     def create
       @shipment = Shipment.where(:id => params[:shipment_id]).first
+      @order = Order.where(:id => params[:ordert_id]).first
       @message = Message.new(message_params)
       # UserMailer.welcome_email(@shipment.user).deliver_later
       if @shipment then @message.shipment = @shipment end
+      if @order then @message.order = @order end
 
       if (@message.sender == current_user || @message.recipient == current_user) && @message.save && @shipment
           # @messages = @shipment.messages
@@ -17,8 +19,9 @@ module Api
 
     def index
       @shipment = Shipment.where(:id => params[:shipment_id]).first
+      @order = Order.where(:id => params[:order_id]).first
       if @shipment then @messages = @shipment.messages else @messages = [] end
-      if @messages.length > 0 && (@shipment.user == current_user || @shipment.order.user == current_user)
+      if @messages.length > 0 && (@shipment.user == current_user || @order.user == current_user)
           render 'index'
        else
          render json: {messsage:'No messages found'}, status: 404
