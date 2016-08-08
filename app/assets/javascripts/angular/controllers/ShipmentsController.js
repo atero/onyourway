364a7e3dfa952ea8ba4dd44300319fcfde19f610
Karen Chobanyan <karen@atero.solutions>
2016-08-08 14:35:35 +0400

    shipments

--- app/assets/javascripts/angular/controllers/ShipmentsController.js
+++ app/assets/javascripts/angular/controllers/ShipmentsController.js
@@ -1,4 +1,4 @@
-App.controller('ShipmentsCtrl',['$scope','$state','localuser', 'shipments', function($scope,$state, localuser, shipments){
+App.controller('ShipmentsCtrl',['$scope','$state','localuser', 'shipments', 'Message', function($scope,$state, localuser, shipments, Message){
 
 	if (localuser) $scope.inherit.user = localuser;
 
