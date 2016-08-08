4e0034ccf7896d8f08fee2b0a2cea8eced3dabff
Karen Chobanyan <karen@atero.solutions>
2016-08-08 14:19:43 +0400

    shipments

--- app/assets/javascripts/angular/controllers/ShipmentsController.js
+++ app/assets/javascripts/angular/controllers/ShipmentsController.js
@@ -12,4 +12,16 @@ App.controller('ShipmentsCtrl',['$scope','$state','localuser', 'shipments', func
 
 	}
 	console.log($scope.local.shipments);
+
+	$scope.sendMessage = function(order,shipment){
+		message = {text:$scope.local.new_message.text, sender:$scope.inherit.user._id, recipient: order.user.id}
+		$scope.inherit.loading = true
+		console.log(shipment);
+		Message.create({shipment_id:shipment.id}, {message:message}, function(res) {
+			$scope.inherit.loading = false
+			shipment.messages.push(res)
+			$scope.local.new_message.text = '';
+		})
+	}
+
 }]);
