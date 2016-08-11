d6460f74a4c1aa24756d7641f289978893b40f96
Karen Chobanyan <karen@atero.solutions>
2016-08-11 17:40:02 +0400

    shipments

--- app/assets/javascripts/angular/controllers/OrdersController.js
+++ app/assets/javascripts/angular/controllers/OrdersController.js
@@ -7,6 +7,43 @@ App.controller('OrdersCtrl', ['$scope', '$state', 'localuser', 'orders', 'Shipme
   }
   console.log($scope.local.orders);
 
+  function stripeResponseHandler(status, response) {
+    // Grab the form:
+    var $form = $('#payment-form');
+
+    if (response.error) { // Problem!
+
+      // Show the errors on the form:
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('.submit').prop('disabled', false); // Re-enable submission
+
+    } else { // Token was created!
+
+      // Get the token ID:
+      var token = response.id;
+
+      // Insert the token ID into the form so it gets submitted to the server:
+      $form.append($('<input type="hidden" name="stripeToken">').val(token));
+
+      // Submit the form:
+      $form.get(0).submit();
+    }
+  };
+
+  $(function() {
+    var $form = $('#payment-form');
+    $form.submit(function(event) {
+      // Disable the submit button to prevent repeated clicks:
+      $form.find('.submit').prop('disabled', true);
+
+      // Request a token from Stripe:
+      Stripe.card.createToken($form, stripeResponseHandler);
+
+      // Prevent the form from being submitted:
+      return false;
+    });
+  });
+
   $scope.accept_travel = function(order, shipment) {
     order.accepted_shipment = shipment.id;
     shipment.status = "accepted-" + order.id;
@@ -70,17 +107,17 @@ App.controller('OrdersCtrl', ['$scope', '$state', 'localuser', 'orders', 'Shipme
 
     console.log(shipment);
 
-			$scope.inherit.loading = true
-      Message.create({
-        shipment_id: shipment.id,
-				order_id: order.id
-      }, {
-        message: message
-      }, function(res) {
-        $scope.inherit.loading = false;
-        shipment.messages.push(res);
-        $scope.local.new_message.text = '';
-      })
+    $scope.inherit.loading = true
+    Message.create({
+      shipment_id: shipment.id,
+      order_id: order.id
+    }, {
+      message: message
+    }, function(res) {
+      $scope.inherit.loading = false;
+      shipment.messages.push(res);
+      $scope.local.new_message.text = '';
+    })
 
   }
 
