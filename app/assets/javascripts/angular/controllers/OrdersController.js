0381a7cf6c9f202a50cae969a0943ac593c56d43
Karen Chobanyan <karen@atero.solutions>
2016-08-11 19:42:40 +0400

    shipments

--- app/assets/javascripts/angular/controllers/OrdersController.js
+++ app/assets/javascripts/angular/controllers/OrdersController.js
@@ -31,7 +31,6 @@ App.controller('OrdersCtrl', ['$scope', '$state', 'localuser', 'orders', 'Shipme
     }
   };
 
-  $(function() {
     var $form = $('#payment-form');
     $form.submit(function(event) {
       // Disable the submit button to prevent repeated clicks:
@@ -39,11 +38,10 @@ App.controller('OrdersCtrl', ['$scope', '$state', 'localuser', 'orders', 'Shipme
 
       // Request a token from Stripe:
       Stripe.card.createToken($form, stripeResponseHandler);
-
+      
       // Prevent the form from being submitted:
       return false;
     });
-  });
 
   $scope.accept_travel = function(order, shipment) {
 
