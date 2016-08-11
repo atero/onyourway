41bcf3cd35e6f4450f3b914623f619809c210258
Karen Chobanyan <karen@atero.solutions>
2016-08-11 16:16:02 +0400

    shipments

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,6 +3,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def stripe_connect
     # Delete the code inside of this method and write your own.
     # The code below is to show you where to access the data.
-    raise request.env["omniauth.auth"].to_yaml
+    # raise request.env["omniauth.auth"].to_yaml
   end
 end
