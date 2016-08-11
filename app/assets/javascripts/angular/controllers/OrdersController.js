a71ee29396437296e951e7d8a5679301df2d5eda
Karen Chobanyan <karen@atero.solutions>
2016-08-11 20:28:48 +0400

    shipments

--- app/assets/javascripts/angular/controllers/OrdersController.js
+++ app/assets/javascripts/angular/controllers/OrdersController.js
@@ -8,43 +8,7 @@ App.controller('OrdersCtrl', ['$scope', '$state', 'localuser', 'orders', 'Shipme
   }
   console.log($scope.local.orders);
 
-  function stripeResponseHandler(status, response) {
-    // Grab the form:
-    var $form = $('#payment-form');
-    console.log('Status:');
-    console.log(status);
-    console.log('Response:');
-    console.log(response);
 
-    if (response.error) { // Problem!
-
-      // Show the errors on the form:
-      $form.find('.payment-errors').text(response.error.message);
-      $form.find('.submit').prop('disabled', false); // Re-enable submission
-
-    } else { // Token was created!
-
-      // Get the token ID:
-      var token = response.id;
-
-      // Insert the token ID into the form so it gets submitted to the server:
-      $form.append(jQuery('<input type="hidden" name="stripeToken">').val(token));
-      // Submit the form:
-      $form.get(0).submit();
-    }
-  };
-
-    var $form = jQuery('#payment-form');
-    $form.submit(function(event) {
-      // Disable the submit button to prevent repeated clicks:
-      $form.find('.submit').prop('disabled', true);
-
-      // Request a token from Stripe:
-      Stripe.card.createToken($form, stripeResponseHandler);
-
-      // Prevent the form from being submitted:
-      return false;
-    });
 
   $scope.accept_travel = function(order, shipment) {
 
