3fff5c3cb6bb129ad76abbb57fffcb032ae9d301
Karen Chobanyan <karen@atero.solutions>
2016-08-12 11:09:40 +0400

    shipments

--- app/assets/javascripts/angular/controllers/StripeController.js
+++ app/assets/javascripts/angular/controllers/StripeController.js
@@ -1,37 +1,6 @@
 App.controller('StripeCtrl',['$scope','$uibModalInstance', "payment_details", function($scope,$uibModalInstance,payment_details){
-	$scope.local={
-		shipment: {
-			order_id: order_id,
-			from: null,
-			to: null
-		}
-	}
-	$scope.payment_details;
-	$scope.ok = function(){
-		if($scope.local.validated){
-			$uibModalInstance.close($scope.local.shipment)
-		}else{
-			$scope.local.validated = true
-		}
-	}
 
-	$scope.cancel = function(){
-		if($scope.local.validated){
-			$scope.local.validated = false
-		}else{
-			$uibModalInstance.close()
-		}
-	}
+	$scope.payment_details = payment_details;
 
-	$scope.countrylist = countrylist
-
-    $scope.local.result = '';
-
-    $scope.local.options = {
-      country: '',
-      types: '(cities)'
-    };
-
-    $scope.local.details = '';
 
 }]);
