ba1b0785533b90989034fb2c17933ab66aadc9f2
Karen Chobanyan <karen@atero.solutions>
2016-08-16 12:33:49 +0400

    deploy

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -1,2 +1,2 @@
-var api_endpoint="https://shopnship.herokuapp.com/api/"
-// var api_endpoint="http://shop-n-ship.herokuapp.com/api/"
+ var api_endpoint="https://www.onyourway.io"
+// var api_endpoint="https://shopnship.herokuapp.com/api/"
