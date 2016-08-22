b567c285b4eb8a49b938be79ea2d1ec8bd8f964b
Karen Chobanyan <karen@atero.solutions>
2016-08-22 12:51:57 +0400

    data

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,4 +9,14 @@ class UserMailer < ApplicationMailer
     @url = 'https://shopnship.herokuapp.com'
     mail(to: email, subject: 'Welcome to OnYourWay')
   end
+
+  def message_email(email, name)
+    @email = email
+    @first_name = name
+    p '************************NEW MESSAGE**************************'
+
+    @url = 'https://shopnship.herokuapp.com'
+    mail(to: email, subject: 'New Message on OnYourWay')
+  end
+
 end
