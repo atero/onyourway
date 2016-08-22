039fa72a06267fbf2251de8a1e3947cc7b7e7a3d
Karen Chobanyan <karen@atero.solutions>
2016-08-22 19:55:39 +0400

    data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
 	def home
 		@user = current_user
 		p '1212122212121212121212221&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
-		@user.photo = user.photo.url(:square)
+		@user.photo = @user.photo.url(:square)
 		p @user.photo
 	end
 
