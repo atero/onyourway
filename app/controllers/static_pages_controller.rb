cc421eaa4f562b4c809acc92efa132337d5aedef
Karen Chobanyan <karen@atero.solutions>
2016-08-22 19:47:31 +0400

    data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,8 @@
 class StaticPagesController < ApplicationController
 
-	@user = current_user
-	p '1212122212121212121212221&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
 	def home
-
+		@user = current_user
+		p '1212122212121212121212221&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
 	end
 
 end
