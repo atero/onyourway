c0d989c139892a5296fc89fdfb009a5fdc37f572
Karen Chobanyan <karen@atero.solutions>
2016-08-23 12:21:08 +0400

    data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,9 @@ class SessionsController < Devise::SessionsController
     sign_in(resource_name, resource)
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource) do |format|
-      resource.photo_url = resource.photo.url
       format.json { render json: resource } # this code will get executed for json request
       p '88888888888888888888888888888888888888'
-      p resource.photo
+      p resource.photo.url
       p '88888888888888888888888888888888888888'
     end
   end
