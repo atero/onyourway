5de6489ed0f2a9bcc6097b3982e09f676f7e2c40
Karen Chobanyan <karen@atero.solutions>
2016-08-23 10:08:23 +0400

    data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  devise_for :users, :controllers => { sessions: 'sessions', :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   post '/users/sign_up.json', to: 'users#create'
   post '/payment', to: 'payment#create'
 
