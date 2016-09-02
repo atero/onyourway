58b8f28e79821968ffc0388ece9d95007587409e
Karen Chobanyan <karen@atero.solutions>
2016-09-02 09:41:09 +0400

    Fixes

---
+++ app/assets/javascripts/angular/directives/datepickerformat.js
@@ -0,0 +1,11 @@
+App
+.directive('datepickerPopup', function (){
+    return {
+        restrict: 'EAC',
+        require: 'ngModel',
+        link: function(scope, element, attr, controller) {
+      //remove the default formatter from the input directive to prevent conflict
+      controller.$formatters.shift();
+  }
+}
+});
