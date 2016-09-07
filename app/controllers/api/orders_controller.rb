131fadf0c6730e365463404ffb7ad261e1fdf98b
Karen Chobanyan <karen@atero.solutions>
2016-09-07 18:38:40 +0400

    Token Code

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -46,6 +46,7 @@ module Api
     def update
       @order = Order.where(id: params[:order_id]).first
       if @order && @order.update(order_params)
+        
         p '999999999999999999999999999999999999999999'
         p order_params
         p '9999999999999999999999999999999999999999999'
