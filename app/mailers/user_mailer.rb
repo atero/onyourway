ea980a6aa88d4eebd27b5b959a2165e53b465709
Karen Chobanyan <karen@atero.solutions>
2016-09-07 18:27:52 +0400

    Token Code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -43,7 +43,7 @@ class UserMailer < ApplicationMailer
     p '************************NEW MESSAGE - GET YOUR TOKEN**************************'
 
     @url = 'https://shopnship.herokuapp.com'
-    mail(to: email, subject: 'OnYourWay - Get your token')
+    mail(to: email, subject: 'OnYourWay - Payment recieved')
 
   end
 
