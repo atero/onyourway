157382b282d942f524c65d672d839440986ff1f6
Susanna Kotoshyan <susanna@atero.solutions>
2016-09-15 11:52:28 +0400

    Fixed Orders for no order state

--- app/assets/javascripts/angular/controllers/OrdersController.js
+++ app/assets/javascripts/angular/controllers/OrdersController.js
@@ -7,14 +7,7 @@ App.controller('OrdersCtrl', ['$scope', '$state', 'localuser', 'orders', 'Shipme
     conversation_shipment: null
   }
 
-  if (orders.length > 0) {
-    $scope.local.data_to_show = orders[0];
-    if ($scope.local.data_to_show.shipments) {
-      if ($scope.local.data_to_show.shipments.length > 0) {
-        $scope.local.conversation_shipment = $scope.local.data_to_show.shipments[0];
-      }
-    }
-  }
+
 
 
   console.log($scope.local.orders);
