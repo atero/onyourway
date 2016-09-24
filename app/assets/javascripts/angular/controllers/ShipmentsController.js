7216f591c13427f5d9e4ab8f4d9a67d0c61af404
Karen Chobanyan <karen@atero.solutions>
2016-09-24 10:12:04 +0400

    Responsive

--- app/assets/javascripts/angular/controllers/ShipmentsController.js
+++ app/assets/javascripts/angular/controllers/ShipmentsController.js
@@ -7,7 +7,7 @@ App.controller('ShipmentsCtrl',['$scope','$state','localuser', 'shipments', 'Ord
 	if (shipments){
 		if (shipments.length > 0){
 			$scope.local.shipmen_to_show = shipments[0];
-			if(shipments[0].orders.length > 0){
+			if(shipments[0].orders && shipments[0].orders.length > 0){
 				$scope.local.data_to_show = shipments[0].orders[0];
 			}
 		}
