eab18084cc3280828c3af98a103f57fe1c8d6900
Susanna Kotoshyan <susanna@atero.solutions>
2016-10-10 17:38:00 +0400

    Change text

--- app/assets/javascripts/angular/controllers/OrdersController.js
+++ app/assets/javascripts/angular/controllers/OrdersController.js
@@ -17,17 +17,17 @@ App.controller('OrdersCtrl', ['$scope', '$state', 'localuser', 'orders', 'Shipme
       }
     }
   }
-
-  if($scope.local.data_to_show.shipments.length > 0) {
-  var refused = 0;
-  for (var i = 0; i < $scope.local.data_to_show.shipments.length; i++) {
-    if ($scope.local.data_to_show.shipments[i].status == "refused") {
-      refused++;
-    }
-  }
-    console.log(refused);
-  $scope.loc = $scope.local.data_to_show.shipments.length - refused;
-}
+// 
+//   if($scope.local.data_to_show.shipments.length > 0) {
+//   var refused = 0;
+//   for (var i = 0; i < $scope.local.data_to_show.shipments.length; i++) {
+//     if ($scope.local.data_to_show.shipments[i].status == "refused") {
+//       refused++;
+//     }
+//   }
+//     console.log(refused);
+//   $scope.local.data_to_show.shipments.length = $scope.local.data_to_show.shipments.length - refused;
+// }
   console.log($scope.local.orders);
 
 
