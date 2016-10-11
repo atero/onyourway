8555083b90f8b4b7a1c9ecc4834a330f85abaa14
Susanna Kotoshyan <susanna@atero.solutions>
2016-10-11 12:07:48 +0400

    Change text

--- app/assets/javascripts/angular/controllers/OrdersController.js
+++ app/assets/javascripts/angular/controllers/OrdersController.js
@@ -14,21 +14,22 @@ App.controller('OrdersCtrl', ['$scope', '$state', 'localuser', 'orders', 'Shipme
     if ($scope.local.data_to_show.shipments) {
       if ($scope.local.data_to_show.shipments.length > 0) {
         $scope.local.conversation_shipment = $scope.local.data_to_show.shipments[0];
+        if($scope.local.data_to_show.shipments.length > 0) {
+          var refused = 0;
+          for (var i = 0; i < $scope.local.data_to_show.shipments.length; i++) {
+            if ($scope.local.data_to_show.shipments[i].status == "refused") {
+              refused++;
+            }
+          }
+          console.log(refused);
+        $scope.local.data_to_show.shipments.length = $scope.local.data_to_show.shipments.length - refused;
+        // $scope.count_offer = $scope.local.data_to_show.shipments.length - refused;
+        }
       }
     }
   }
 
-  if($scope.local.data_to_show.shipments.length > 0) {
-    var refused = 0;
-    for (var i = 0; i < $scope.local.data_to_show.shipments.length; i++) {
-      if ($scope.local.data_to_show.shipments[i].status == "refused") {
-        refused++;
-      }
-    }
-    console.log(refused);
-  $scope.local.data_to_show.shipments.length = $scope.local.data_to_show.shipments.length - refused;
-//  $scope.count_offer = $scope.local.data_to_show.shipments.length - refused;
-}
+
   console.log($scope.local.orders);
 
 
