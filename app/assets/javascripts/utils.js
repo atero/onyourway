5da0b407e32ca27570333045940641b7cc1b01e4
Karen Chobanyan <karen@atero.solutions>
2016-10-27 11:30:56 +0400

    Sender photo

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -1,2 +1,2 @@
-// var api_endpoint="https://www.onyourway.io/api/"
-var api_endpoint="https://shopnship.herokuapp.com/api/"
+var api_endpoint="https://www.onyourway.io/api/"
+// var api_endpoint="https://shopnship.herokuapp.com/api/"
