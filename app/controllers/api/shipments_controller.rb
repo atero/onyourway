c3ca9b6b2c42157bcfd4b08c854445a516631d9a
Susanna Kotoshyan <susanna@atero.solutions>
2016-12-02 13:50:07 +0400

    Rejected_email

--- app/controllers/api/shipments_controller.rb
+++ app/controllers/api/shipments_controller.rb
@@ -67,7 +67,14 @@ module Api
       if @shipment && @shipment.update(shipment_params)
         p '8888888888888888888888888888888888888888'
         p shipment_params
-        # @shipment.order.save
+        @shipment.order.save
+        if @shipment.refuses != params[:shipment][:refuses]
+          UserMailer.rejected_email(@traveler.email, @traveler.first_name, @shoper.first_name).deliver_later
+        end
+        # if  @shipment.refuses_changed? #=> true
+        #   UserMailer.rejected_email(@traveler.email, @traveler.first_name, @shoper.first_name).deliver_later
+        # end
+
         render json: @shipment, status: :accepted
       else
         render json: { messsage: 'No orders found' }, status: 404
