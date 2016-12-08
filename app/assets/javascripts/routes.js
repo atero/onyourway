e178013541192174bb42783d9de60c7bda990ef8
Susanna Kotoshyan <susanna@atero.solutions>
2016-12-08 10:45:01 +0400

    Meta

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -278,6 +278,6 @@ window.App.config(['$stateProvider', '$urlRouterProvider','$locationProvider',
       $locationProvider.html5Mode(true);
   }
 ])
-// App.run(['ngMeta', function(ngMeta) {
-//   ngMeta.init();
-// }]);
+App.run(['ngMeta', function(ngMeta) {
+  ngMeta.init();
+}]);
