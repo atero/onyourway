b2c3576c8646f2f2cee8398b8b8d8f8112edbd35
Susanna Kotoshyan <susanna@atero.solutions>
2016-12-08 10:36:22 +0400

    Meta

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,1 @@
-window.App = angular.module("ShopNShip", ['google.places', 'ngResource','ui.router','ui.bootstrap','Devise','ngRoute','templates','ngSanitize','ngCsv','ngAnimate','oitozero.ngSweetAlert','mwl.calendar','angularFileUpload', 'ngMeta'])
-  .run(['ngMeta', function(ngMeta) {
-    ngMeta.init();
-  }]);
+window.App = angular.module("ShopNShip", ['google.places', 'ngResource','ui.router','ui.bootstrap','Devise','ngRoute','templates','ngSanitize','ngCsv','ngAnimate','oitozero.ngSweetAlert','mwl.calendar','angularFileUpload', 'ngMeta']);
