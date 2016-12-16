3c0de630a56627c3d07d87dfe0a5807ce1d6d814
Susanna Kotoshyan <susanna@atero.solutions>
2016-12-16 10:54:23 +0400

    Refuses

--- app/controllers/api/shipments_controller.rb
+++ app/controllers/api/shipments_controller.rb
@@ -69,7 +69,11 @@ module Api
         p shipment_params
         # @shipment.order.save
        if @shipment.has_attribute?(:refuses)
-           UserMailer.rejected_email().deliver_later
+         @order = Order.where(id: order_id).first
+         @shipment = Shipment.where(id: shipment_id).first
+         @shoper = User.where(id: @order.user_id).first
+         @traveler = User.where(id: @shipment.user_id).first
+             UserMailer.rejected_email(@traveler.email, @traveler.first_name, @shoper.first_name).deliver_later
          end
         render json: @shipment, status: :accepted
       else
