f8c86bd942394912c8a66daafba41b647be7c82c
Karen Chobanyan <karen@atero.solutions>
2016-12-19 14:13:52 +0400

    prerender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.middleware.use Rack::Prerender
+  config.middleware.use Rack::Prerender, prerender_token: 'DaAiocqMKXRBDuwcxVoA'
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
