5404f27c4276bfb6995cc8832b137c2851df5ead
Susanna Kotoshyan <susanna@atero.solutions>
2016-12-20 11:46:01 +0400

    Add token_email

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -46,13 +46,12 @@ module Api
     def update
       @order = Order.where(id: params[:order_id]).first
       if order_params_tok['confirm_token'] && order_params_tok['confirm_token'] == @order.accepted_token
-        shipment_id = params[:shipment_id]
-        # shipment_id = params[:accepted_shipment]
+        shipment_id = @order.accepted_shipment
         @shipment = Shipment.where(id: shipment_id).first
         # @traveler = User.where(id: @shipment.user_id).first
         @ord_par = order_params_tok
         @ord_par['status'] = 'delivered'
-        UserMailer.confirm_email(@shipment.first_name).deliver_later
+        UserMailer.confirm_email(@shipment.user.first_name).deliver_later
       end
 
       if @order && @order.update(@ord_par)
