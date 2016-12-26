edd8459d0c6d40dfc0927a8ce07539514de998e0
Susanna Kotoshyan <susanna@atero.solutions>
2016-12-26 11:48:54 +0400

    save imgage

--- app/assets/javascripts/angular/controllers/ProfileController.js
+++ app/assets/javascripts/angular/controllers/ProfileController.js
@@ -34,7 +34,8 @@ App.controller('ProfileCtrl', ['$scope', '$state', 'localuser', 'User', function
     }, {
       user: $scope.inherit.user
     }, function(res) {
-      $scope.inherit.loading = true;
+      $scope.inherit.loading = false;
+      location.reload();
       //$scope.inherit.goState('pr');
     })
   };
