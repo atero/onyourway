2f213820bc84bf84a2080629aa41ff6d63558541
Susanna Kotoshyan <susanna@atero.solutions>
2016-12-29 11:08:47 +0400

    Search google api

--- app/assets/javascripts/angular/controllers/ProfileController.js
+++ app/assets/javascripts/angular/controllers/ProfileController.js
@@ -35,9 +35,10 @@ App.controller('ProfileCtrl', ['$scope', '$state', 'localuser', 'User', function
       user: $scope.inherit.user
     }, function(res) {
       $scope.inherit.loading = false;
-      location.reload();
+
       //$scope.inherit.goState('pr');
     })
+    location.reload();
   };
 
 }]);
