4a22c348b3f915c571aa88999b240d4673a166c3
Susanna Kotoshyan <susanna@atero.solutions>
2016-12-29 12:18:55 +0400

    Message image show

--- app/assets/javascripts/angular/controllers/ShipmentsController.js
+++ app/assets/javascripts/angular/controllers/ShipmentsController.js
@@ -24,7 +24,7 @@ App.controller('ShipmentsCtrl',['$scope','$state','localuser', 'shipments', 'Ord
 		console.log($scope.local.new_message.text);
 		message = {text:$scope.local.new_message.text, sender:shipment.user.id, recipient: order.user.id}
 		// $scope.inherit.loading = true
-		Message.create({shipment_id:shipment.id, order_id:order.id}, {message:message}, function(res) {
+		Message.create({shipment_id:shipment.id, order_id:order.id, shipment_photo: shipment.user.photo}, {message:message}, function(res) {
 			$scope.inherit.loading = false
 			shipment.messages.push(res)
 			console.log(res);
