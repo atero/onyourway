838abd595a2afb45a7f3c812714d5ddab2ccceec
Susanna Kotoshyan <susanna@atero.solutions>
2017-01-09 11:18:53 +0400

    Search api

--- app/assets/javascripts/angular/controllers/ResultsController.js
+++ app/assets/javascripts/angular/controllers/ResultsController.js
@@ -25,16 +25,20 @@ App.controller('ResultsCtrl', ['$scope', '$state', "$uibModal", 'localuser', 'or
     filtered = orders
       $scope.search.from = $scope.search.from.address_components[0].long_name;
       $scope.search.to = $scope.search.to.address_components[0].long_name;
-    if ($scope.search.from && $scope.search.from != "") {
-      filtered = filtered.filter(function(d) {
-        return d.from.toLowerCase().indexOf($scope.search.from.toLowerCase()) > -1
-      })
-    }
-    if ($scope.search.to && $scope.search.to != "") {
-      filtered = filtered.filter(function(d) {
-        return d.to.toLowerCase().indexOf($scope.search.to.toLowerCase()) > -1
-      })
-    }
+      if ($scope.search.from.address_components[0].long_name && $scope.search.from.address_components[0].long_name != "") {
+        if ($scope.search.from && $scope.search.from != "") {
+          filtered = filtered.filter(function(d) {
+            return d.from.toLowerCase().indexOf($scope.search.from.toLowerCase()) > -1
+          })
+        }
+      }
+      if ($scope.search.to.address_components[0].long_name && $scope.search.to.address_components[0].long_name != "") {
+        if ($scope.search.to && $scope.search.to != "") {
+          filtered = filtered.filter(function(d) {
+            return d.to.toLowerCase().indexOf($scope.search.to.toLowerCase()) > -1
+          })
+        }
+      }
     return filtered
   }
 
