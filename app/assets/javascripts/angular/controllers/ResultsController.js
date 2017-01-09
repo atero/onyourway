272ff8914a6b3d164451fa91214ae13b8326d923
Susanna Kotoshyan <susanna@atero.solutions>
2017-01-09 17:52:57 +0400

    Search api

--- app/assets/javascripts/angular/controllers/ResultsController.js
+++ app/assets/javascripts/angular/controllers/ResultsController.js
@@ -23,6 +23,7 @@ App.controller('ResultsCtrl', ['$scope', '$state', "$uibModal", 'localuser', 'or
   }
   $scope.filterResults = function() {
     filtered = orders
+    console.log($scope.search);
     if (!!$scope.search.from.address_components[0].long_name && $scope.search.from.address_components[0].long_name != "" ) {
           $scope.search.from = $scope.search.from.address_components[0].long_name;
        }
